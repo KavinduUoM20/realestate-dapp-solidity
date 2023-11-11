8e6a8b0e9ee3e4f0711c62bafd837cf83ab7068a
_im'kavindu <65580465+KavinduUoM20@users.noreply.github.com>
2023-11-11 21:58:08 +0530

    Initial Commit

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,5 +6,36 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer,seller,inspector, lender
+    let realEstate,escrow
+    it('saves the address', async()=>{
 
+        // Setup accounts
+        [buyer, seller, inspector, lender] = await ethers.getSigners()
+        
+        //Deploy - Real Estate Contract
+        const RealEstate = await ethers.getContractFactory('RealEstate')
+        realEstate = await RealEstate.deploy()
+
+        //MINT
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait()
+
+        //Deploy - Escrow Contract
+        const Escrow = await ethers.getContractFactory('Escrow')
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address
+        )
+
+        //Check NFT Address
+        let result = await escrow.nftAddress()
+        expect(result).to.be.equal(realEstate.address)
+
+        result = await escrow.seller()
+        expect(result).to.be.equal(seller.address)
+
+    })
 })
